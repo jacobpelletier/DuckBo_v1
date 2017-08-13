adc32bf55d664c3156cc4d7669cb374ecd027ad3
Jacob Pelletier <jacob.pelletier@pondurance.com>
2017-08-13 12:54:11 -0400

    Forgot to upload the files with 'em, whoops

---
+++ Assets/Scripts/BloodSplatterLife.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplatterLife : MonoBehaviour {
+
+	public float lifetime = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Lifetime");
+	}
+
+	IEnumerator Lifetime(){
+		yield return new WaitForSeconds(lifetime);
+		Destroy(gameObject);
+	}
+}
