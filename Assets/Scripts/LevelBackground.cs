cd9e6e05a438968873307fc842d734eb70170191
Jacob Pelletier <jacob.pelletier@pondurance.com>
2017-08-29 09:37:04 -0400

    Fixed a weird fidgeting of background

--- Assets/Scripts/LevelBackground.cs
+++ Assets/Scripts/LevelBackground.cs
@@ -29,7 +29,7 @@ public class LevelBackground : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		float variance = cam.transform.position.x - origPos;
 		variance *= speedReduce;
