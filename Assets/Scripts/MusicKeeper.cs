d6d8c7a60bd035c00b91e319b550bea0b659e09f
Jacob Pelletier <jacob.pelletier@pondurance.com>
2017-08-29 23:46:57 -0400

    Tons of fun stuffz

--- Assets/Scripts/MusicKeeper.cs
+++ Assets/Scripts/MusicKeeper.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class MusicKeeper : MonoBehaviour {
 
+	public static MusicKeeper musicControl;
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad(gameObject);
+		if(musicControl == null){
+			DontDestroyOnLoad(gameObject);
+			musicControl = this;
+		}
+		else if(musicControl != null){
+			Destroy(gameObject);
+		}
 	}
 }
